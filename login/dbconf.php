acd5ac2410dc4fcaffb5340fa46ca4f277c1e2d3
Florian Page <fpage@inovaxo.fr>
2016-10-04 16:17:41 +0200

    t

--- login/dbconf.php
+++ login/dbconf.php
@@ -1,8 +1,8 @@
 <?php
 //DATABASE CONNECTION VARIABLES
 $host = "localhost"; // Host name
-$username = "Florian"; // Mysql username
-$password = "EWapCk5yn-YcQ7)u9Q22/;@L2.78^a"; // Mysql password
+$username = "root"; // Mysql username
+$password = ""; // Mysql password
 $db_name = "site_lya"; // Database name
 
 //DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
