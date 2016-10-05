0811fe64f0e7b7906dfa305158d2027bbbe024f4
Florian Page <fpage@inovaxo.fr>
2016-10-05 15:30:20 +0200

    s

--- ajout_images_bdd.php
+++ ajout_images_bdd.php
@@ -8,14 +8,15 @@ catch (Exception $e)
         exit('Erreur : ' . $e->getMessage());
 }
   //on commence par parcourir le répertoire
-  $dir = '/volume1/Florian/Photos/Lya/';
+  $dir = '/volume1/Florian/Photos/lya_sav/';
   $files = scandir($dir);
   for($i=0;$i<count($files);$i++){
       if(strtolower(pathinfo($files[$i],PATHINFO_EXTENSION)) == 'jpg'){
-        $req = $pdo->prepare("INSERT INTO images (chemin) VALUES (:chemin)");
+        $req = $pdo->prepare("INSERT INTO images (chemin,miniature) VALUES (:chemin,:miniature)");
         $req->execute(
                    array(
-                         "chemin" => "./photos/".$files[$i]
+                         "chemin" => "./photos/".$files[$i],
+                         "miniature" => "./photos/thumb_".$files[$i]
                         )
                      );
 
